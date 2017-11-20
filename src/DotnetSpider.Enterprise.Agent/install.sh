97d19f77e86cdc8d75b43a632391469e09a513e0
jackical <jackical.shen@research.xbmail.com.cn>
2017-11-20 19:21:23 +0800

    udpate

--- src/DotnetSpider.Enterprise.Agent/install.sh
+++ src/DotnetSpider.Enterprise.Agent/install.sh
@@ -1,20 +1,16 @@
 ﻿if [ -n "$1" ]; then
-		if [ -f "/opt/DotnetSpider.Agent/agent.lock" ]; then
-			echo "agent is ruuning."
-		else
-			rm /opt/DotnetSpider.Agent -rf
-			packageUrl="http://nasabigdata.com:30012/contents/dotnetspider.enterprise/$1"
-			echo $packageUrl
-			rm /tmp/$1 -f
-			echo "remove /tmp/$1 success"
-			wget http://nasabigdata.com:30012/contents/dotnetspider.enterprise/$1  -O /tmp/$1
-			echo "download http://nasabigdata.com:30012/contents/dotnetspider.enterprise/$1 success"
-			unzip -o /tmp/$1  -d /opt
-			rm /opt/DotnetSpider.Agent/config.ini -f
-			wget http://nasabigdata.com:30012/contents/dotnetspider.enterprise/config.ini -O /opt/DotnetSpider.Agent/config.ini
-			cd /opt/DotnetSpider.Agent
-			nohup dotnet DotnetSpider.Enterprise.Agent.dll --daemon &
-		fi
+	rm /opt/DotnetSpider.Agent -rf
+	packageUrl="http://nasabigdata.com:30012/contents/dotnetspider.enterprise/$1"
+	echo $packageUrl
+	rm /tmp/$1 -f
+	echo "remove /tmp/$1 success"
+	wget http://nasabigdata.com:30012/contents/dotnetspider.enterprise/$1  -O /tmp/$1
+	echo "download http://nasabigdata.com:30012/contents/dotnetspider.enterprise/$1 success"
+	unzip -o /tmp/$1  -d /opt
+	rm /opt/DotnetSpider.Agent/config.ini -f
+	wget http://nasabigdata.com:30012/contents/dotnetspider.enterprise/config.ini -O /opt/DotnetSpider.Agent/config.ini
+	cd /opt/DotnetSpider.Agent
+	nohup dotnet DotnetSpider.Enterprise.Agent.dll --daemon &
 else
 echo "version missing."
 fi
